0778adbca1e637bfc2cd41da6b3ad01c589d41e0
Satya Ananda <dev@satyaand.com>
2024-12-09 11:24:44 +0700

    fix(final): latest progress

--- Glove_Code/src/main.cpp
+++ Glove_Code/src/main.cpp
@@ -1,15 +1,27 @@
 #include <setup.h>
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
-  
+
   pinMode(LED_BUILTIN, OUTPUT);
   Setup::ESPNOW();
   Setup::MPU6050();
   Setup::Buttons();
   Setup::SpeechRecognition();
+
+  for (int i = 0; i < 10; i++)
+  {
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(200);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(200);
+    Serial.println("Jalan");
+  }
+
   Setup::InitialTask();
 }
- 
-void loop() {
+
+void loop()
+{
 }
--- VAIO_Code/src/main.cpp
+++ VAIO_Code/src/main.cpp
@@ -1,21 +1,25 @@
 #include "setup.h"
 #include <Arduino.h>
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
 
   Setup::Wifi();
   Setup::ESPNOW();
-  Setup::WebServer();
+  // Setup::WebServer();
   Setup::LEDIndicators();
   Setup::Motors();
-  //  Setup::Ultrasonic();
-  //  Setup::Servo();
+  Setup::Ultrasonic();
+  Setup::Servo();
   // Setup::DS4();
+  pinMode(vacuumPin, OUTPUT);
+  digitalWrite(vacuumPin, HIGH);
   Setup::InitialTask();
 
   vTaskDelete(NULL);
 }
 
-void loop() {
+void loop()
+{
 }
